a8f032642a73b6abb46b938a4d572a8357d30a9e
Programador Novato <info@programadornovato.com>
2020-08-04 05:17:00 -0500

    Insertar elementos en nuestra interfaz gráfica
    Video:
    https://youtu.be/NVHbuMpL9es

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 from tkinter import *
+from tkinter import ttk
 
 class Alumno:
     def __init__(self,ventana):
         self.ventana=ventana
         self.ventana.title("Hola mundo")
-        
+        marco=LabelFrame(self.ventana,text="Alumno")
+        marco.grid(row=0,column=0,columnspan=3,pady=20)
+        #Nombre
+        Label(marco,text="Nombre").grid(row=0,column=0)
+        Entry(marco).grid(row=0,column=1)
+        #Clave
+        Label(marco,text="Clave").grid(row=1,column=0)
+        Entry(marco).grid(row=1,column=1)
+        #Boton
+        ttk.Button(marco,text="Guardar alumno").grid(row=2,columnspan=2,sticky=W+E)
 if __name__=="__main__":
     ventana=Tk()
     aplicacion=Alumno(ventana)
