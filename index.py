a066d1d2533aa95faf7f4e08396cd3abaa3192b1
Programador Novato <info@programadornovato.com>
2020-08-04 02:26:11 -0500

    Initial commit

---
+++ index.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class Alumno:
+    def __init__(self,ventana):
+        self.ventana=ventana
+        self.ventana.title("Hola mundo")
+if __name__=="__main__":
+    ventana=Tk()
+    aplicacion=Alumno(ventana)
+    ventana.mainloop()
