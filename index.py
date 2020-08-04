eb2936d8ecee1bbcb4e96aa33547860c5922637c
Programador Novato <info@programadornovato.com>
2020-08-04 02:28:15 -0500

    Hola Mundo con Python y TKinter🐣
    Video:
    https://www.youtube.com/watch?v=9imqvtAGcqk&list=PLCTD_CpMeEKQBfbGkWHmc2TlMdOlWjla7&index=5&t=0s

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ class Alumno:
     def __init__(self,ventana):
         self.ventana=ventana
         self.ventana.title("Hola mundo")
+        
 if __name__=="__main__":
     ventana=Tk()
     aplicacion=Alumno(ventana)
