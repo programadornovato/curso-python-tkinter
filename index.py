ad81ab8b3781a367104bab8c0531154dc64a9bca
Programador Novato <info@programadornovato.com>
2020-08-04 13:14:37 -0500

    Crear registros
    Video:
    https://youtu.be/QIoyTYBGhvw

--- index.py
+++ index.py
@@ -9,19 +9,25 @@ class Alumno:
         marco.grid(row=0,column=0,columnspan=3,pady=20)
         #Nombre
         Label(marco,text="Nombre").grid(row=0,column=0)
-        Entry(marco).grid(row=0,column=1)
+        self.nombre=Entry(marco)
+        self.nombre.grid(row=0,column=1)
+        self.nombre.focus()
         #Clave
         Label(marco,text="Clave").grid(row=1,column=0)
-        Entry(marco).grid(row=1,column=1)
+        self.clave=Entry(marco)
+        self.clave.grid(row=1,column=1)
         #Boton
-        ttk.Button(marco,text="Guardar alumno",command=self.mostrarDatos).grid(row=2,columnspan=2,sticky=W+E)
+        ttk.Button(marco,text="Guardar alumno",command=self.agregarRegistro).grid(row=2,columnspan=2,sticky=W+E)
+        #Mensaje
+        self.mensaje=Label(text='',fg='green')
+        self.mensaje.grid(row=3,column=0,columnspan=2,sticky=W+E)
         #Tabla
         self.tabla=ttk.Treeview(self.ventana,columns=2)
         self.tabla.grid(row=4,column=0,columnspan=2)
         self.tabla.heading("#0",text="Nombre",anchor=CENTER)
         self.tabla.heading("#1",text="Clave",anchor=CENTER)
 
-    def consulataAlumnos(self,query):
+    def queryAlumnos(self,query):
         try:
             conn=mariadb.connect(
                 host="localhost",
@@ -38,10 +44,20 @@ class Alumno:
         registros=self.tabla.get_children()
         for registro in registros:
             self.tabla.delete(registro)
-        cur=self.consulataAlumnos("SELECT `nombre`,`clave` FROM `alumnos`")
+        cur=self.queryAlumnos("SELECT `nombre`,`clave` FROM `alumnos`")
         for (nombre,clave) in cur:
             self.tabla.insert('',0,text=nombre,values=clave)
-
+    def agregarRegistro(self):
+        if len(self.nombre.get())!=0 and len(self.clave.get())!=0:
+            query="INSERT INTO `alumnos` (`id`, `nombre`, `clave`) VALUES (NULL, '"+self.nombre.get()+"', '"+self.clave.get()+"');"
+            self.queryAlumnos(query)
+            self.mensaje['text']="El alumno "+self.nombre.get()+" se a insertado exitosamente"
+            self.nombre.delete(0,END)
+            self.clave.delete(0,END)
+            self.nombre.focus()
+        else:
+            self.mensaje['text']="El nombre y la clave del alumno no pueden estar vacias humano tonto"
+        self.mostrarDatos()
 if __name__=="__main__":
     ventana=Tk()
     aplicacion=Alumno(ventana)
